2ba251fb0a4c8152c60adfcab8ed2b6303bc020b
Luft <15201225365@163.com>
2021-08-25 11:33:37 +0800

    add_shaderInfo

--- FrameDataOutput.py
+++ FrameDataOutput.py
@@ -9,7 +9,8 @@ from docx.oxml.ns import nsdecls
 from docx.oxml import parse_xml
 from enum import Enum
 from PIL import Image
-from docx.shared import Cm
+from docx.shared import Cm, Inches, Pt
+
 
 # 每一帧的数据类
 class DrawCallData:
@@ -35,18 +36,18 @@ class DrawCallData:
 
 
 class ImageType(Enum):
-    TEXTURE2D = 1
-    MIPMAP = 2
-    CUBEMAP = 4
-    TEXTURE2DARRAY = 8
-    TEXTURE3D = 16
+    T2D = 1         #TEXTURE2D
+    MIP = 2         #MIPMAP
+    CUBE = 4        #CUBEMAP
+    T2DA = 8        #TEXTURE2DARRAY
+    T3D = 16        #TEXTURE3D
 
 
 class ImageInfo:
     def __init__(self):
         self.imagePath = ""
         self.imageName = ""
-        self.imageType = ImageType.TEXTURE2D
+        self.imageType = ImageType.T2D
         self.size = (-1, -1)
         self.isFrameCapture = False
 
@@ -88,23 +89,26 @@ def getImageInfo(imageFoldPath, fileName: str):
         files = os.listdir(fullPath)
         imageInfo.imagePath = fullPath + "\\" + files[0]
         if fileName.__contains__("(Mipmap)"):
-            imageInfo.imageType = ImageType.MIPMAP
+            imageInfo.imageType = ImageType.MIP
             imageInfo.imageName = fileName.strip("(Mipmap)")
         elif fileName.__contains__("(Cubemap)"):
-            imageInfo.imageType = ImageType.CUBEMAP
+            imageInfo.imageType = ImageType.CUBE
             imageInfo.imageName = fileName.strip("(Cubemap)")
         elif fileName.__contains__("(Texture3D)"):
-            imageInfo.imageType = ImageType.TEXTURE3D
+            imageInfo.imageType = ImageType.T3D
             imageInfo.imageName = fileName.strip("(Texture3D)")
         elif fileName.__contains__("(Texture2DArray)"):
-            imageInfo.imageType = ImageType.TEXTURE2DARRAY
+            imageInfo.imageType = ImageType.T2DA
             imageInfo.imageName = fileName.strip("(Texture2DArray)")
     else:
-        imageInfo.imagePath = fullPath
-        imageInfo.imageName = fileName.strip(".png")
-        imageInfo.imageType = ImageType.TEXTURE2D
-        if fileName.startswith("DrawCall_"):
-            imageInfo.isFrameCapture = True
+        if fileName.endswith(".png"):
+            imageInfo.imagePath = fullPath
+            imageInfo.imageName = fileName.strip(".png")
+            imageInfo.imageType = ImageType.T2D
+            if fileName.startswith("DrawCall_"):
+                imageInfo.isFrameCapture = True
+        else:
+            return None
     if imageInfo.imagePath != "":
         img = Image.open(imageInfo.imagePath)
         imageInfo.size = img.size
@@ -191,24 +195,34 @@ def getTopDrawCall(csv_path, word_path, topNum, Matrix, frameResPath):
         dataTable.cell(i + 1, highLightIdx)._tc.get_or_add_tcPr().append(shading_elm2)
 
     # 单个DrawCall数据处理
+    fontSize = Pt(10)
     for i in range(topNum):
         dc = allDrawCalls[i]
-        document.add_heading("DrawCall-" + str(dc.ID), level=1)
+        document.add_heading("Top " + str(i), level=2)
+        document.add_paragraph("DrawCall " + str(dc.ID))
         imageInfoList = getDrawCallImages(dc.ID, frameResPath)
-        dcTable = document.add_table(2, 3, style="Light Grid")
+        dcTable = document.add_table(2, 3, style="Table Grid")
+        dcTable.width = Cm(9.2) + Cm(3) + Cm(3.2)
+        dcTable.autofit = False
+        dcTable.allow_autofit = False
+        widths = (Cm(9.2), Cm(3), Cm(3.2))
+        for row in dcTable.rows:
+            for idx, width in enumerate(widths):
+                row.cells[idx].width = width
         dcTable.alignment = WD_TABLE_ALIGNMENT.CENTER  # 居中
-        dcTable.cell(0,0).text = "帧截图"
-        dcTable.cell(0,1).text = "相关纹理"
-        dcTable.cell(0,2).text = "GPU数据"
+        dcTable.cell(0, 0).text = "帧截图"
+        dcTable.cell(0, 1).text = "渲染相关资源"
+        dcTable.cell(0, 2).text = "GPU数据"
+
         isFirstImage = True
         for imageInfo in imageInfoList:
             if imageInfo.isFrameCapture:
                 pr1 = dcTable.cell(1,0).paragraphs[0].add_run()
                 pic = pr1.add_picture(imageInfo.imagePath)
-                scale1 = imageInfo.size[0] / 5
+                scale1 = imageInfo.size[0] / 8.8
                 pic.height = Cm(imageInfo.size[1]/scale1)
-                pic.width = Cm(5)
-                dcTable.cell(1, 0).add_paragraph("Size [{0}x{1}]".format(imageInfo.size[0], imageInfo.size[1]))
+                pic.width = Cm(8.8)
+                dcTable.cell(1, 0).add_paragraph("{0}x{1}".format(imageInfo.size[0], imageInfo.size[1]))
             else:
                 p1 = None
                 if isFirstImage:
@@ -219,14 +233,31 @@ def getTopDrawCall(csv_path, word_path, topNum, Matrix, frameResPath):
                 pic = p1.add_run().add_picture(imageInfo.imagePath)
                 pic.height = Cm(2)
                 pic.width = Cm(2)
-                p1.add_run("{0} [({1}),{2}x{3}]".format(imageInfo.imageName, imageInfo.imageType.name, imageInfo.size[0], imageInfo.size[1]))
+                p1.add_run("\n{0}({1})\n{2}x{3}\n".format(imageInfo.imageName, imageInfo.imageType.name, imageInfo.size[0], imageInfo.size[1])).font.size = fontSize
 
         drawCall = allDrawCalls[i]
-        dcTable.cell(1, 2).paragraphs[0].add_run("Read Total:"+getTaleValueStr(drawCall.ReadTotal, drawCallSum.ReadTotal))
-        dcTable.cell(1, 2).paragraphs[0].add_run("\nWrite Total:" + getTaleValueStr(drawCall.WriteTotal, drawCallSum.WriteTotal))
-        dcTable.cell(1, 2).paragraphs[0].add_run("\nClocks:" + getTaleValueStr(drawCall.Clocks, drawCallSum.Clocks))
-        dcTable.cell(1, 2).paragraphs[0].add_run("\nTexture Read:" + getTaleValueStr(drawCall.TextureRead, drawCallSum.TextureRead))
-        dcTable.cell(1, 2).paragraphs[0].add_run("\nVertex Read:" + getTaleValueStr(drawCall.VertexRead, drawCallSum.VertexRead))
+        text = dcTable.cell(1, 2).paragraphs[0].add_run("Read:\n"+getTaleValueStr(drawCall.ReadTotal, drawCallSum.ReadTotal) +
+                                    "\nWrite:\n" + getTaleValueStr(drawCall.WriteTotal, drawCallSum.WriteTotal) +
+                                    "\nClocks:\n" + getTaleValueStr(drawCall.Clocks, drawCallSum.Clocks) +
+                                    "\nTexture:\n" + getTaleValueStr(drawCall.TextureRead, drawCallSum.TextureRead) +
+                                    "\nVertex:\n" + getTaleValueStr(drawCall.VertexRead, drawCallSum.VertexRead))
+        text.font.size = fontSize
+
+        #Shader Info
+        drawCallDataPath = os.path.abspath(frameResPath + "\\" + str(drawCall.ID)) + "\\DrawCallData.txt"
+        if os.path.exists(drawCallDataPath):
+            f = open(drawCallDataPath)
+            shaderInfoStr = f.readlines()[0]
+            shaderInfoStr = shaderInfoStr.strip('ID').strip()
+            shaderStart = shaderInfoStr.find('/') + 1
+            shaderEnd = shaderInfoStr.find(', ')
+            shaderName = shaderInfoStr[shaderStart:shaderEnd-1]
+            programProperties = shaderInfoStr[shaderEnd+2:].split(', ')
+            p = dcTable.cell(1, 1).add_paragraph()
+            p.add_run("Shader：{0}\n".format(shaderName)).font.size = fontSize
+            p.add_run("\nProperties：\n").font.size = fontSize
+            for prop in programProperties:
+                p.add_run("{0}\n".format(prop)).font.size = fontSize
 
     document.save(word_path)
     print("Save SDFrameData")
