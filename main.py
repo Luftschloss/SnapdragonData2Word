a50c16293531df2925e66987e6e051867addd799
yufei <15201225365@163.com>
2022-03-03 21:50:28 +0800

    update

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ timePeriods = None
 timePeriods = [(120, 680)]
 # 分割线区间段
 slicedPeriods = None
-# slicedPeriods = [25, 46, 52, 83, 109, 136, 156, 180, 243, 258, 282, 303, 330, 348, 361, 380]
+slicedPeriods = [25, 46, 52, 83, 109, 136, 156, 180, 243, 258, 282, 303, 330, 348, 361, 380]
 
-# word = WordOutput.createSnapDragonDataDocx("22.csv", "22.docx", timePeriods, slicedPeriods)
+word = WordOutput.createSnapDragonDataDocx("SKZY.csv", "SKZY.docx", timePeriods, slicedPeriods)
 
 frameResourcePath = "..\\Snapdragon2Word\\FrameCapture"
 # "Read Total (Bytes)"、“Clocks”
-frameDoc = FrameDataOutput.getTopDrawCall("22.csv", "22.docx", 20, "Clocks", frameResourcePath)
+# frameDoc = FrameDataOutput.getTopDrawCall("22.csv", "22.docx", 20, "Clocks", frameResourcePath)
